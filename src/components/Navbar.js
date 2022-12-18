06a4e2d79c332abfbc904c7dd1c2ecf094a34b46
Gudisa Gebi <gudisagebi1@gmail.com>
2022-12-17 16:36:20 -0800

    Reacter Router Updated Version to be Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="btn">
-        Home
-      </Link>
-      <Link to="/about" className="btn">
-        About
-      </Link>
-      <Link to="/products" className="btn">
-        Products
-      </Link>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/products">Products</Link>
     </nav>
   );
 };
